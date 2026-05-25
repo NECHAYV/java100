df60aefaffefd47d7f57f24dba12ac0a5a690292
Titov Ilya <titov1608@gmail.com>
2026-05-25 09:44:43 +0300

    init

---
+++ src/main/java/org/exemple/demojava100/Demojava100Application.java
@@ -0,0 +1,13 @@
+package org.exemple.demojava100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demojava100Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demojava100Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/exemple/demojava100/Demojava100ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.exemple.demojava100;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demojava100ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
